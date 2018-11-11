4e9ef7eb3c5c2c108eef754ad2d256adc21a0600
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-11-11 13:17:47 +0000

    Add testing application

---
+++ tests/testapp/TestApplication.hpp
@@ -0,0 +1,15 @@
+/// @file
+
+#pragma once
+
+#include "Application.hpp"
+
+namespace silvia
+{
+
+class cTestApplication final : public cApplication
+{
+public:
+};
+
+}; // namespace silvia
---
+++ tests/testapp/main.cpp
@@ -0,0 +1,14 @@
+/// @file
+
+#include <cstddef>
+
+#include "TestApplication.hpp"
+
+int SilviaMain(int argc, char **argv)
+{
+	silvia::CTestApplication App;
+	
+	App.Run();
+	
+	return EXIT_SUCCESS;
+};
